6803c7fe93de2e658c696b8adb605e366fe28252
noribang <hoonk9387@gmail.com>
2020-01-19 07:11:45 -0600

    Added post-css loader and plugins.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const postCSSPlugins = [
+	require('postcss-simple-vars'),
+	require('postcss-nested'),
+	require('autoprefixer')
+];
 
 module.exports = {
 	entry: './app/assets/scripts/App.js',
@@ -12,7 +17,7 @@ module.exports = {
 		rules : [
 			{
 				test: /\.css$/i,
-				use: ['style-loader', 'css-loader']
+				use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
 			}
 		]
 	},	
